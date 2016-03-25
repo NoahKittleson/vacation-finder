038b1937d4907cc1cf11b5e7f5f067109da5eae3
Noah Kittleson <noahkittleson@gmail.com>
2016-03-25 09:23:01 -0700

    wrote out the rest of questions with very simple output

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,20 @@ $(document).ready(function() {
     event.preventDefault();
     var result1 = $("#transportation").val();
     var result2 = $("input:radio[name=temperature]:checked").val();
-    if (result1 === "Select One" || !result2) {
+    var result3 = $("input:radio[name=sandwich]:checked").val();
+    var result4 = $("#events").val();
+    var result5 = $("#departure").val();
+    var result6 = $("#name").val();
+    if (!result1 || !result2 || !result3 || !result4 || !result5 || !result6) {
       alert("BAD INPUT")
     }
     else {
       $(".output").append(result1 + "\n");
       $(".output").append(result2 + "\n");
+      $(".output").append(result3 + "\n");
+      $(".output").append(result4 + "\n");
+      $(".output").append(result5 + "\n");
+      $(".output").append(result6 + "\n");
     }
   })
 });
